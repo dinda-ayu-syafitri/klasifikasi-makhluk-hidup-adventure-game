d89c266e8d8d12ed472101dabd2db4df2e6c2c09
dinda-ayu-syafitri <dindasyafitri@gmail.com>
2024-04-09 23:12:01 +0700

    Add: Emblem Script

---
+++ Assets/Scripts/Emblem.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Emblem : MonoBehaviour
+{
+    bool collected = false;
+   
+   private void OnTriggerEnter(Collider other) {
+    if(!collected)
+    {
+        CollectEmblem();
+    }
+   }
+
+   private void CollectEmblem()
+   {
+       collected = true;
+       Destroy(gameObject);
+       print("Emblem collected!");
+   }
+}
