e515c588da3eedd6df05b2bd5da78f86be1b11d2
dinda-ayu-syafitri <dindasyafitri@gmail.com>
2024-04-12 01:03:47 +0700

    Add: Save player position

--- Assets/Scripts/DataPersistence/Data/GameData.cs
+++ Assets/Scripts/DataPersistence/Data/GameData.cs
@@ -8,10 +8,12 @@ public class GameData
 {
     public int totalPoints;
     public int emblemCollected;
+    public Vector3 playerPosition;
 
     public GameData()
     {
         this.totalPoints = 0;
         this.emblemCollected = 0;
+        this.playerPosition = new Vector3(20f, 4.5f, -53f);
     }
 }
