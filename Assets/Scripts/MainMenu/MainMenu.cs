7c4fb787e06ab3e2e24076616ff15eda51727d26
dinda-ayu-syafitri <dindasyafitri@gmail.com>
2024-04-12 22:03:29 +0700

    Add: Main Menu Scene

---
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void OnNewGameClicked()
+    {
+        Debug.Log("New Game Clicked");
+    }
+    public void OnContinueGame()
+    {
+        Debug.Log("Continue Game Clicked");
+    }
+}
